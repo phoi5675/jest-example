ac4d2591a682fbf93ff59f15aaf5a675200c1135
phoi5675 <phoi5675@gmail.com>
2024-07-19 21:00:43 +0900

    Add @types/eslint for eslint flat config file

--- expressjs/eslint.config.mjs
+++ expressjs/eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
+/** @type { import("eslint").Linter.FlatConfig[] } */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
