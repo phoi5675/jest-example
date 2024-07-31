670cf1687ff19d715a304368eb3285fe92b08c42
phoi5675 <phoi5675@gmail.com>
2024-07-31 21:03:31 +0900

    Write test case for loginService

--- expressjs/src/router/auth/login/__test__/loginService.test.ts
+++ expressjs/src/router/auth/login/__test__/loginService.test.ts
@@ -3,4 +3,14 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-// TODO: Add more tests.
+describe(`Login service test`, () => {
+  it(`should return token and login time if the user is in database`, async () => {
+    // TODO:
+  });
+  it(`should return undefined if the user exists, but password is wrong`, async () => {
+    // TODO:
+  });
+  it(`should return undefined if the user is not in database`, async () => {
+    // TODO:
+  });
+});
