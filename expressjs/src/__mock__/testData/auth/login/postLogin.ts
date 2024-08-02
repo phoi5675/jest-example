e3ccceff80e1fb8f2c272598af24a2e20e331646
phoi5675 <phoi5675@gmail.com>
2024-08-02 21:00:14 +0900

    Add test code for `loginService`

--- expressjs/src/__mock__/testData/auth/login/postLogin.ts
+++ expressjs/src/__mock__/testData/auth/login/postLogin.ts
@@ -5,10 +5,13 @@
 
 import { PostLoginReq } from "@/router/auth/login/types/PostLogin";
 import { mockReq } from "@/shared/utils/__test__/mockMiddleware";
+import { encryptByPublicKey } from "@/shared/utils/crypto";
+
+const encryptedPassword = encryptByPublicKey("password");
 
 export const postLoginReq = mockReq<PostLoginReq>({
   body: {
     username: "username",
-    password: "password",
+    password: encryptedPassword,
   },
 });
--- expressjs/src/router/auth/login/__test__/loginService.test.ts
+++ expressjs/src/router/auth/login/__test__/loginService.test.ts
@@ -3,14 +3,74 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+import { addUser } from "@/mock/setups/auth/login/addUser";
+import { deleteUsers } from "@/mock/teardowns/auth/login/deleteUser";
+import { loginService } from "@/router/auth/login/loginService";
+import { PostLoginResHeader } from "@/router/auth/login/types/PostLogin";
+import { isValidRes } from "@/shared/utils/__test__/jestUtil";
+import { encryptByPublicKey } from "@/shared/utils/crypto";
+import { postLoginReq } from "@/testData/auth/login/postLogin";
+import Joi from "joi";
+
 describe(`Login service test`, () => {
+  beforeEach(async () => {
+    await addUser();
+  });
+  afterEach(async () => {
+    await deleteUsers();
+  });
   it(`should return token and login time if the user is in database`, async () => {
-    // TODO:
+    const req = postLoginReq;
+
+    const validator = Joi.object<PostLoginResHeader>({
+      token: Joi.string().required(),
+      "logined-at": Joi.string().isoDate().required(),
+    });
+
+    const res = await loginService.postLogin(req);
+
+    const isValid = isValidRes<PostLoginResHeader>(validator, res);
+
+    expect(isValid).toBeTruthy();
   });
   it(`should return undefined if the user exists, but password is wrong`, async () => {
-    // TODO:
+    const req = postLoginReq;
+
+    const password = encryptByPublicKey(`someWrongPassword`);
+
+    req.body.username = postLoginReq.body.username;
+    req.body.password = password;
+
+    const validator = Joi.object<PostLoginResHeader>({
+      token: Joi.string().required(),
+      "logined-at": Joi.string().isoDate().required(),
+    });
+
+    const res = await loginService.postLogin(req);
+
+    const isValid = isValidRes<PostLoginResHeader>(validator, res);
+
+    expect(isValid).toBeFalsy();
+    expect(res).toBeUndefined();
   });
   it(`should return undefined if the user is not in database`, async () => {
-    // TODO:
+    const req = postLoginReq;
+
+    const password = encryptByPublicKey(postLoginReq.body.password);
+
+    req.body.username = `not_existed_user`;
+    req.body.password = password;
+
+    const validator = Joi.object<PostLoginResHeader>({
+      token: Joi.string().required(),
+      "logined-at": Joi.string().isoDate().required(),
+    });
+
+    const res = await loginService.postLogin(req);
+
+    const isValid = isValidRes<PostLoginResHeader>(validator, res);
+
+    expect(isValid).toBeFalsy();
+    expect(res).toBeUndefined();
   });
 });
