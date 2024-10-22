00334fdba1d5e15fb9c36f53f73dc3e5fc95edfc
phoi5675 <phoi5675@gmail.com>
2024-10-22 11:15:18 +0900

    Fix alias problem in webpack

--- reactjs/next.config.mjs
+++ reactjs/next.config.mjs
@@ -1,4 +1,16 @@
+import path from "path";
+
+const __dirname = path.resolve();
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: {
+    resolve: {
+      alias: {
+        "@/": path.resolve(__dirname, "./src/"),
+      },
+    },
+  },
+};
 
 export default nextConfig;
